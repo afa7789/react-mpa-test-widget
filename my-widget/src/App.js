c5092b30fcf33bf9ed3cd0f2074cd6a94f58d4e3
Arthur Abeilice <afa7789@gmail.com>
2022-09-19 20:16:28 -0300

    not sure can't replicate

--- my-widget/src/App.js
+++ my-widget/src/App.js
@@ -1,20 +1,49 @@
 import './App.css';
 import React from "react";
 import ResetCSS from './resetCss';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 console.log("There")
 
 function App() {
   return (
-    <div>
-      <ResetCSS/>
-      <div style={{
-        backgroundColor:"Black"
-      }}>
-        <h1> HELLO WORLD h1 </h1>
-      </div>
-      <h2> HELLO WORLD h2 </h2>
-    </div>
+    <>
+      <ResetCSS />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={
+            <div>
+              <div style={{
+                backgroundColor: "Black"
+              }}>
+                <h1> HELLO WORLD h1 ALGO </h1>
+              </div>
+              <h2> HELLO WORLD h2 </h2>
+            </div>} 
+          />
+          <Route exact path="/about" element={
+            <div>
+              <div style={{
+                backgroundColor: "Black"
+              }}>
+                <h1> HELLO WORLD h1 ABOUT </h1>
+              </div>
+              <h2> HELLO WORLD h2 </h2>
+            </div>} 
+          />
+          <Route exact path="/test" element={
+            <div>
+              <div style={{
+                backgroundColor: "Black"
+              }}>
+                <h1> HELLO WORLD h1 TEST </h1>
+              </div>
+              <h2> HELLO WORLD h2 </h2>
+            </div>} 
+          />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
